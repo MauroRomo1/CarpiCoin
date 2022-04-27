c587592623acd89ed01fc69d25866b2ef33bbf02
Mauro Romo <mauroromo1999@outlook.com>
2022-04-27 19:35:43 -0300

    Terminando la pagina de not found

--- src/pages/ErrorScreen.jsx
+++ src/pages/ErrorScreen.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import carpiError from "../img/footer-logo/capri-error.png";
+
 const ErrorScreen = () => {
-  return <div>ErrorScreen</div>;
+  return (
+    <div className="container text-center my-3">
+      <div className="row">
+        <div className="col">
+          <h1 className="titulo">Page Not Found</h1>
+          <img className="img-fluid" src={carpiError} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorScreen;
